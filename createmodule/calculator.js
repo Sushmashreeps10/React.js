2cbcfd22f4de5925c086193cca76ed78a6042bdb
Sushmashreeps10 <sushre17@gmail.com>
2025-04-02 11:26:35 +0530

    update: git commit -m update

---
+++ createmodule/calculator.js
@@ -0,0 +1,10 @@
+export let sumData =(a , b) => {
+    return a+b;
+}
+
+export let SubData =(a , b) => {
+    return a-b;
+}
+
+export default (sumData, SubData)
+// export default SubData
---
+++ createmodule/index.js
@@ -0,0 +1,4 @@
+import sumData from "./calculator.js";
+
+console.log(sumData(100,20));
+// console.log(subData(100,20));
